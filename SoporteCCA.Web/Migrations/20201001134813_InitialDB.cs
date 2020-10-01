ad0f6ea22ea576c686beac911e58dc1dd51d7c74
Carlos Lomeli <sackrone@hotmail.com>
2020-10-01 08:50:54 -0500

    Complete Data Base Part 2

---
+++ SoporteCCA.Web/Migrations/20201001134813_InitialDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoporteCCA.Web.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Modeloramas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Key = table.Column<string>(maxLength: 3, nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    CommissionAgent = table.Column<string>(maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modeloramas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Modeloramas");
+        }
+    }
+}
